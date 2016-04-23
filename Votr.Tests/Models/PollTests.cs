058ab77c717f2dc6783e4bf2936af278b562de85
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-04-23 10:52:36 -0500

    added sanity check test, poll and option models

---
+++ Votr.Tests/Models/PollTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Votr.Models;
+
+namespace Votr.Tests.Models
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class PollTests
+    {
+       
+
+        [TestMethod]
+        public void PollEnsureICanCreateInstance()
+        {
+            Poll p = new Poll();
+            Assert.IsNotNull(p);
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
---
+++ Votr/Models/Poll.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Votr.Models
+{
+    public class Poll
+    {
+        int PollId { get; set; }
+
+        [MaxLength(500)]
+        [Required]
+        string Title { get; set;}
+
+        [Required]
+        DateTime     Startdate { get; set; }
+
+        [Required]
+        DateTime EndDate { get; set; }
+
+        //need Options Relation
+        public virtual ICollection<Option> Options { get; set; }
+        //Tag Relation
+        //User Relation
+        //vote Relation
+    }
+}
