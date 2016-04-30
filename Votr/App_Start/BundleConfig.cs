01e32033c1b9c90789e6c020f8a4f41c845e3781
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-04-30 14:01:49 -0500

    Pushing commits from end of class.  Installed and injected in angular

--- Votr/App_Start/BundleConfig.cs
+++ Votr/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace Votr
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Sctripts/app.js",
+                "~/Sctripts/angular.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
